b98d31b54b815cfe32dcea95a8ed55f15c39d638
Xayw3 <xaywe1@gmail.com>
2023-03-09 14:54:11 +0200

    change audio

--- src/components/Player/data.ts
+++ src/components/Player/data.ts
@@ -4,6 +4,12 @@ import image3 from '../../assets/cover/image-4-min.jpg';
 import image4 from '../../assets/cover/image-7-min.jpg';
 import image5 from '../../assets/cover/image-9-min.jpg';
 import image6 from '../../assets/cover/image-6-min.jpg';
+import audio1 from '../../assets/audio/Felix Jaehn & Ray Dalton_Call It Love.m4a';
+import audio2 from "../../assets/audio/Lil Nas X_STAR WALKIN'.m4a";
+import audio3 from "../../assets/audio/Rag'N'Bone Man_Human.mp3";
+import audio4 from '../../assets/audio/Oliver Tree & Robin Schulz_Miss-You.mp3';
+import audio5 from '../../assets/audio/Imagine Dragons JID_Enemy.m4a';
+import audio6 from '../../assets/audio/Linkin Park_Lost.m4a';
 
 const songData = [
   {
@@ -12,7 +18,7 @@ const songData = [
     track: 'Call It Love',
     favorited: true,
     cover: image1,
-    song: './audio/Felix Jaehn & Ray Dalton_Call It Love.m4a',
+    song: audio1,
   },
   {
     link: 'https://www.youtube.com/watch?v=ykWEo-G5W9Q',
@@ -20,7 +26,7 @@ const songData = [
     track: 'STAR WALKIN',
     favorited: false,
     cover: image2,
-    song: "./audio/Lil Nas X_STAR WALKIN'.m4a",
+    song: audio2,
   },
   {
     link: 'https://www.youtube.com/watch?v=L3wKzyIN1yk',
@@ -28,7 +34,7 @@ const songData = [
     track: 'Human',
     favorited: false,
     cover: image3,
-    song: "./audio/Rag'N'Bone Man_Human.mp3",
+    song: audio3,
   },
   {
     link: 'https://www.youtube.com/watch?v=BX0lKSa_PTk',
@@ -36,7 +42,7 @@ const songData = [
     track: 'Miss You',
     favorited: false,
     cover: image4,
-    song: './audio/Oliver Tree & Robin Schulz_Miss-You.mp3',
+    song: audio4,
   },
   {
     link: 'https://www.youtube.com/watch?v=hHB1Ikzfpmc',
@@ -44,7 +50,7 @@ const songData = [
     track: 'Enemy',
     favorited: true,
     cover: image5,
-    song: './audio/Imagine Dragons JID_Enemy.m4a',
+    song: audio5,
   },
   {
     link: 'https://www.youtube.com/watch?v=7NK_JOkuSVY',
@@ -52,7 +58,7 @@ const songData = [
     track: 'Lost',
     favorited: true,
     cover: image6,
-    song: './audio/Linkin Park_Lost.m4a',
+    song: audio6,
   },
 ];
 
